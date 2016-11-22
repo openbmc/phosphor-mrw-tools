e6d8cc4a3e102d01081faa213790d63252c0c87b
Matt Spinler <spinler@us.ibm.com>
2016-11-22 11:19:26 -0600

    Create LinuxHelpers.pm
    This perl module will contain helpers to translate from
    MRW to Linux numbering/naming.
    
    Change-Id: Ie652a2b05cf7a0269e5c4c573f6ad570477d3030
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>

---
+++ LinuxHelpers.pm
@@ -0,0 +1,27 @@
+package LinuxHelpers;
+
+use constant MRW_TO_LINUX_I2C_BUS_NUM_OFFSET => -1;
+
+1;
+
+=head1 NAME
+
+LinuxHelpers
+
+=head1 DESCRIPTION
+
+Contains helpers for translation from MRW nomenclature to Linux
+nnomenclature.
+
+=head1 CONSTANTS
+
+=over 4
+
+=item MRW_TO_LINUX_I2C_BUS_NUM_OFFSET
+
+The value to add to an I2C bus number in the MRW to get the
+bus number used in Linux.
+
+=back
+
+=cut
