a93e2097d17e4b8d0eae48c477c12a6f03278e75
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2017-09-18 05:50:59 -0500

    Adding pre-req for IPMI assertions
    A pre-req condition can be added for a sensor value,
    for example presence of a unit should be true for
    functional to be true.
    
    Change-Id: I7b87c7ffa29f06160164aba2b39c743f0f9c733f
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>

--- gen_ipmi_sensor.pl
+++ gen_ipmi_sensor.pl
@@ -138,10 +138,20 @@ sub writeToFile
                     #will write property named "Property" first then
                     #other properties.
             print $fh "      ".$dbusProperty.":\n";
-            while (my ( $offset,$values) = each %{$dbusPropertyValue}) {
-                print $fh "          $offset:\n";
-                while (my ( $key,$value) = each %{$values})  {
-                    print $fh "            $key: ". $value."\n";
+            while (my ($offset,$values) = each %{$dbusPropertyValue}) {
+                if ($offset eq "prereq") {
+                    print $fh "          $offset:\n";
+                    while (my ($preOffset,$preValues) = each %{$values}) {
+                        print $fh "            $preOffset:\n";
+                        while (my ($key,$value) = each %{$preValues})  {
+                            print $fh "              $key: ". $value."\n";
+                        }
+                    }
+                } else {
+                    print $fh "          $offset:\n";
+                    while (my ($key,$value) = each %{$values})  {
+                        print $fh "            $key: ". $value."\n";
+                    }
                 }
             }
         }
