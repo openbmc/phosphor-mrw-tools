bc76f6a29bb18382faf090f7809e4f1012b9082f
Gunnar Mills <gmills@us.ibm.com>
2017-09-26 16:19:32 -0500

    Get settings values from the MRW
    Added getting the settings values from the MRW and writing them
    to the output file.
    
    Change-Id: Ic7e163beb4f5d13cae00ff2013b1c4080d39a256
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

--- gen_settings.pl
+++ gen_settings.pl
@@ -10,20 +10,26 @@ my $force           = 0;
 my $serverwizFile  = "";
 my $debug           = 0;
 my $outputFile     = "";
+# Setting to get from the MRW
+my @settings;
 
 # Command line argument parsing
 GetOptions(
 "f"   => \$force,            # numeric
 "i=s" => \$serverwizFile,    # string
 "o=s" => \$outputFile,       # string
+"s=s{1,}" => \@settings,     # array
 "d"   => \$debug,
 )
 or printUsage();
 
-if (($serverwizFile eq "") or ($outputFile eq ""))
+if (($serverwizFile eq "") or ($outputFile eq "") or (@settings == 0))
 {
     printUsage();
 }
+
+# Hashmap of all the settings and their values
+my %settingsHash;
 
 # API used to access parsed XML data
 my $targetObj = Targets->new;
@@ -40,17 +46,49 @@ if($force == 1)
 $targetObj->loadXML($serverwizFile);
 print "Loaded MRW XML: $serverwizFile \n";
 
+# Process all the targets in the XML
+foreach my $target (sort keys %{$targetObj->getAllTargets()})
+{
+    # A future improvement could be to specify the MRW target.
+    next if ("SYS" ne $targetObj->getType($target, "TYPE"));
+
+    foreach my $setting (@settings)
+    {
+        $settingsHash{$setting} = $targetObj->getAttribute($target, $setting);
+    }
+    last;
+}
+generateYamlFile();
+
+sub generateYamlFile
+{
+    my $fileName = $outputFile;
+    open(my $fh, '>', $fileName) or die "Could not open file '$fileName' $!";
+
+    foreach my $setting (sort keys %settingsHash)
+    {
+        # YAML with list of {Setting:Value} dictionary
+        print $fh "- Setting: ";
+        print $fh "$setting\n";
+        print $fh "  Value: ";
+        print $fh "$settingsHash{$setting}\n";
+    }
+    close $fh;
+}
+
 # Usage
 sub printUsage
 {
     print "
-    $0 -i [XML filename] -o [Output filename] [OPTIONS]
+    $0 -i [XML filename] -o [Output filename] -s [MRW Settings] [OPTIONS]
+    
+Required:
+    -i = MRW XML filename
+    -o = YAML output filename
+    -s = space seperated list of setting values to get from the MRW
 Options:
     -f = force output file creation even when errors
     -d = debug mode
-
-PS: mrw::Targets can be found in https://github.com/open-power/serverwiz/
-    mrw::Inventory can be found in https://github.com/openbmc/phosphor-mrw-tools/
     \n";
     exit(1);
 }
