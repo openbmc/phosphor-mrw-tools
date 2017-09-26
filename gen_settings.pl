d3b9f02d406a20fc8bc900fed5261c840889510d
Gunnar Mills <gmills@us.ibm.com>
2017-09-26 16:19:32 -0500

    Get settings values from the MRW
    Added getting the settings values from the MRW and writing them
    to the output file.
    
    Change-Id: Ic7e163beb4f5d13cae00ff2013b1c4080d39a256
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

--- gen_settings.pl
+++ gen_settings.pl
@@ -10,6 +10,9 @@ my $force           = 0;
 my $serverwizFile  = "";
 my $debug           = 0;
 my $outputFile     = "";
+# Setting to get from the MRW
+my @settings = ("OPEN_POWER_SOFT_MIN_PCAP_WATTS",
+                "OPEN_POWER_N_PLUS_ONE_BULK_POWER_LIMIT_WATTS");
 
 # Command line argument parsing
 GetOptions(
@@ -25,6 +28,9 @@ if (($serverwizFile eq "") or ($outputFile eq ""))
     printUsage();
 }
 
+# Hashmap of all the settings and their values
+my %settingsHash;
+
 # API used to access parsed XML data
 my $targetObj = Targets->new;
 if($debug == 1)
@@ -39,6 +45,35 @@ if($force == 1)
 
 $targetObj->loadXML($serverwizFile);
 print "Loaded MRW XML: $serverwizFile \n";
+
+# Process all the targets in the XML
+foreach my $target (sort keys %{$targetObj->getAllTargets()})
+{
+    # The settings are found under the sys-sys-power9 type target instance
+    next if ("sys-sys-power9" ne $targetObj->getTargetType($target, "TYPE"));
+
+    foreach my $setting (@settings)
+    {
+        $settingsHash{$setting} = $targetObj->getAttribute($target, $setting);
+    }
+}
+generateYamlFile();
+
+sub generateYamlFile
+{
+    my $fileName = $outputFile;
+    open(my $fh, '>', $fileName) or die "Could not open file '$fileName' $!";
+
+    foreach my $setting (sort keys %settingsHash)
+    {
+        # YAML with list of {setting:value} dictionary
+        print $fh "- Setting: ";
+        print $fh "$setting\n";
+        print $fh "  Value: ";
+        print $fh "$settingsHash{$setting}\n";
+    }
+    close $fh;
+}
 
 # Usage
 sub printUsage
